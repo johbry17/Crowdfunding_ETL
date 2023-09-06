ec0bb9e2eecfd07a7f8bac109446c515e1e6fb68
johbry17 <73319490+johbry17@users.noreply.github.com>
2023-09-06 00:16:45 -0400

    postgreSQL written, Readme updated

---
+++ confirm_tables.sql
@@ -0,0 +1,13 @@
+-- confirm all tables exist
+
+-- category
+SELECT * FROM category;
+
+-- subcategory
+SELECT * FROM subcategory;
+
+-- campaign
+SELECT * FROM campaign;
+
+-- contacts
+SELECT * FROM contacts;
